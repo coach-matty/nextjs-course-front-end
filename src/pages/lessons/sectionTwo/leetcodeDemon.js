0e05fe79e0c9a0993106ec5f8ff4e6b0a0deb22c
Matthew Violet <matthewviolet@Matthews-MacBook-Pro.local>
2024-06-09 23:19:16 -0400

    leetcode upate

--- src/pages/lessons/sectionTwo/leetcodeDemon.js
+++ src/pages/lessons/sectionTwo/leetcodeDemon.js
@@ -20,9 +20,24 @@ export default function RealisticTimeline() {
     </nav>
     <div style={parentStyle}>
 
-        <h1>workin on it!</h1>
-
+        <h1>LEETCODE YAY</h1>
+        <p>I'l try to keep this short. LeetCode is a deal you make with the devil.
+            If you want to go into interviews with confidence and actually start passing 50%+ of
+            interviews - you need to leetcode. Unless you're just straight up disgusting 
+            at leetcode then you probably need to start doing it up to 6-9 months in advance.
+        </p>
+        <p>Dont' cram! For my most recent position I started doing 1-2 problems a day, Monday - Friday,
+            about 4 months before I started interviewing. I was consistent with a lighter load
+            and wasn't at risk of burning out.
+        </p>
+        <p>It sucks but if you're willing to suck it up it will drastically increase your
+            chances of getting a job.
+        </p>
         
+        <h1>NEETCODE</h1>
+        <p>Luckily this guy named <a href="https://neetcode.io/">NeetCode</a> made the greatest free 
+        leetcode resource to ever exist. I'm not even going to try to teach any leetcode stuff
+        because he already won. Which is a good thing! Let's go!</p>
         </div>
         </>);
 }
